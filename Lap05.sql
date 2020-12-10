7c7913a9ebcd39f492ecaba266f5bb575bad18fa
Thiem <tranthiem1993@gmail.com>
2020-12-11 00:16:56 +0700

    Update Lap05.sql

--- Lap05.sql
+++ Lap05.sql
@@ -101,8 +101,12 @@ VALUES
 (6,'Bui Thi Xuan','1983-08-25',123456789,'F','Hai Duong','2010-12-05',2),
 (7,'Ha Van Thanh','1993-08-25',123456789,'M','Tuyen Quang','2017-12-05',3)
 
+INSERT INTO NhanVien(MaNV,TenNV,NgaySinh,SoCMND,GioiTinh,DiaChi,NgayVaoLam,MaPB)
+VALUES 
+(8,'Nguyen Van Tich','1991-08-25',123456781,'M','Ha Nam','2017-12-05',1)
+
 INSERT INTO LuongDA
-VALUES(3,5,19000,'2017-08-09'),
+VALUES(3,8,19000,'2017-08-09'),
 (4,1,10000,'2016-08-09'),
 (5,4,13000,'2019-08-09'),
 (6,2,10000,'2014-08-09'),
@@ -147,12 +151,32 @@ HAVING COUNT(NhanVien.MaPB) >= ALL (SELECT COUNT(NhanVien.MaPB) AS 'So luong'
 FROM NhanVien INNER JOIN PhongBan ON NhanVien.MaPB = PhongBan.MaPB
 GROUP BY PhongBan.TenPB)
 --11. Tính tổng số lưong của các nhân viên trong phòng Hành chính.
-SELECT NhanVien.MaNV
-FROM NhanVien INNER JOIN PhongBan ON NhanVien.MaPB = PhongBan.MaPB
-WHERE PhongBan.TenPB = 'Hanh Chinh'
+--12. Hiển thị tống lương của các nhân viên có số CMND tận cùng bằng 9.
+SELECT LuongDA.MaNV,NhanVien.TenNV,NhanVien.SoCMND,SUM(LuongDA.SoTien) AS 'Tong luong' 
+FROM NhanVien INNER JOIN LuongDA ON NhanVien.MaNV = LuongDA.MaNV
+WHERE NhanVien.SoCMND LIKE '%9'
+GROUP BY LuongDA.MaNV,NhanVien.TenNV,NhanVien.SoCMND
+--13. Tìm nhân viên có số lương cao nhất.
 SELECT LuongDA.MaNV,NhanVien.TenNV,SUM(LuongDA.SoTien) AS 'Tong luong' 
 FROM NhanVien INNER JOIN LuongDA ON NhanVien.MaNV = LuongDA.MaNV
-WHERE LuongDA.MaNV = ALL(SELECT NhanVien.MaNV
-FROM NhanVien INNER JOIN PhongBan ON NhanVien.MaPB = PhongBan.MaPB
-WHERE PhongBan.TenPB = 'Hanh Chinh')
 GROUP BY LuongDA.MaNV,NhanVien.TenNV
+HAVING SUM(LuongDA.SoTien) >= ALL(SELECT SUM(LuongDA.SoTien) AS 'Tong luong' 
+FROM NhanVien INNER JOIN LuongDA ON NhanVien.MaNV = LuongDA.MaNV
+GROUP BY LuongDA.MaNV,NhanVien.TenNV)
+--14. Tìm nhân viên ở phòng Hành chính có giới tính bằng ‘F’ và có mức lương > 1200000.
+SELECT NhanVien.TenNV,PhongBan.TenPB,NhanVien.GioiTinh 
+FROM NhanVien INNER JOIN PhongBan ON NhanVien.MaPB = PhongBan.MaPB
+WHERE PhongBan.TenPB = 'Hanh Chinh' AND NhanVien.GioiTinh = 'F'
+--15. Tìm tổng lương trên từng phòng.
+--16. Liệt kê các dự án có ít nhất 2 người tham gia.
+SELECT LuongDA.MaDA,COUNT(LuongDA.MaNV) AS 'So luong nguoi tham gia' FROM LuongDA
+GROUP BY LuongDA.MaDA
+HAVING COUNT(LuongDA.MaNV) >= 2
+--17. Liệt kê thông tin chi tiết của nhân viên có tên bắt đầu bằng ký tự ‘N’.
+SELECT * FROM NhanVien
+WHERE NhanVien.TenNV LIKE 'N%'
+--18. Hiển thị thông tin chi tiết của nhân viên được nhận tiền dự án trong năm 2017.
+SELECT * 
+FROM NhanVien INNER JOIN LuongDA ON NhanVien.MaNV = LuongDA.MaNV
+WHERE DATEPART(year,LuongDA.NgayNhan) = '2017'
+--19. Hiển thị thông tin chi tiết của nhân viên không tham gia bất cứ dự án nào.
